55ed8df166d217273da507999efc85dc7e51787a
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-25 02:44:15 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var isPathGlobal = require('./');
+var test = require('ava');
+
+test('check if a path is in PATH', function (t) {
+	t.assert(isPathGlobal('/bin/sh'));
+	t.assert(!isPathGlobal(__filename));
+	t.end();
+});
