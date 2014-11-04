be056b53b8bea61bb987e4afe559e8d955f7267a
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-04 23:56:21 +0100

    More tests

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ var test = require('ava');
 
 test('check if a path is in PATH', function (t) {
 	t.assert(isPathGlobal('/bin/sh'));
+	t.assert(isPathGlobal('/usr/bin'));
 	t.assert(!isPathGlobal(__filename));
 	t.end();
 });
