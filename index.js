5539b39d2971ecb534cf73c710c745371a37661d
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-04 16:41:39 +0100

    Return `true` if `str === path`

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ var isPathInside = require('is-path-inside');
 
 module.exports = function (str) {
 	return process.env.PATH.split(delimiter).some(function (path) {
-		return isPathInside(str, path);
+		return isPathInside(str, path) || str === path;
 	});
 };
