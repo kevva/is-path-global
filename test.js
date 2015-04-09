ee8a55a3f82b175104433f66928474a900ac40bf
Kevin Martensson <kevinmartensson@gmail.com>
2015-04-09 18:18:00 +0200

    Show values of failing tests

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ var test = require('ava');
 var isPathGlobal = require('./');
 
 test('check if a path is in PATH', function (t) {
-	t.assert(isPathGlobal('/bin/sh'));
-	t.assert(isPathGlobal('/usr/bin'));
-	t.assert(!isPathGlobal(__filename));
+	t.assert(isPathGlobal('/bin/sh'), isPathGlobal('/bin/sh'));
+	t.assert(isPathGlobal('/usr/bin'), isPathGlobal('/usr/bin'));
+	t.assert(!isPathGlobal(__filename), isPathGlobal(__filename));
 	t.end();
 });
